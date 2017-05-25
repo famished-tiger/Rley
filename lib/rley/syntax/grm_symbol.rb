290794a03f3d8911d089595a93ffb5a70965ac77
famished.tiger <famished.tiger@yahoo.com>
2017-05-25 22:13:21 +0200

    Missing file in previous commit

--- lib/rley/syntax/grm_symbol.rb
+++ lib/rley/syntax/grm_symbol.rb
@@ -2,28 +2,37 @@ module Rley # This module is used as a namespace
   module Syntax # This module is used as a namespace
     # Abstract class for grammar symbols.
     # A grammar symbol is an element that appears in grammar rules.
-    class GrmSymbol  
+    class GrmSymbol
       # The name of the grammar symbol
       attr_reader(:name)
+
+      # An indicator that tells whether the grammar symbol can generate a
+      # non-empty string of terminals.
+      attr_writer(:generative)
 
       # Constructor.
       # aName [String] The name of the grammar symbol.
       def initialize(aName)
         @name = aName.dup
       end
-      
-      # Return true iff the symbol is a terminal
-      def terminal?()
-        # Default implementation to override if necessary
-        return false
-      end
-      
+
       # The String representation of the grammar symbol
       # @return [String]
       def to_s()
         return name.to_s
       end
-        
+
+      # @return [Boolean] true iff the symbol is a terminal
+      def terminal?()
+        # Default implementation to override if necessary
+        return false
+      end
+
+      # @return [Boolean] true iff the symbol is generative.
+      def generative?()
+        return @generative
+      end
+
     end # class
   end # module
 end # module
